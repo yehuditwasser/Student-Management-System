ea08bae43da7946de8ea12ee95871e56fef3a564
yehuditwasser <ywy0706@gmail.com>
2024-11-05 11:58:12 +0200

    npm primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SmsButtonComponent } from './Components/sms-button/sms-button.component';
+import { SmsInputComponent } from './Components/sms-input/sms-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SmsButtonComponent
+    SmsButtonComponent,
+    SmsInputComponent
   ],
   imports: [
     BrowserModule,
